bfdd83fedfb900ad4eeefba9686225148c915caa
iescheinberg <agr.scheinberg@gmail.com>
2024-11-16 18:58:45 -0300

    app usuario

--- walletApp/settings.py
+++ walletApp/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # My Apps
-    
-    
+    # Mis aplicaciones
+    'apps.usuarios'
 ]
 
 

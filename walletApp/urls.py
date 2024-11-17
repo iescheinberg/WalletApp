295a3bd76c7339a0c72c2105d5668a263b11e478
iescheinberg <agr.scheinberg@gmail.com>
2024-11-17 09:57:54 -0300

    limpiando proyecto

--- walletApp/urls.py
+++ walletApp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as views_django
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]

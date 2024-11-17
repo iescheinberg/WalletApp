a5c37e13e7452ef8fb3272d49ffc5b8cc723dd55
iescheinberg <agr.scheinberg@gmail.com>
2024-11-17 10:38:17 -0300

    creado-app usuarios, mod. db

--- walletApp/settings.py
+++ walletApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Mis aplicaciones
+    'apps.usuarios'
 
 ]
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'walletApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +79,16 @@ WSGI_APPLICATION = 'walletApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',                    
+    }
+}
+
+
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'WalletApp',       
@@ -87,7 +97,7 @@ DATABASES = {
         'HOST': 'localhost',                
         'PORT': '3306',                    
     }
-}
+} """
 
 
 
